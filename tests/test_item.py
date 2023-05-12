c83ee2504153ec473a8b82fae15218d0aee5fe18
pozdniakova olga <caramelca07@mail.ru>
2023-05-13 01:41:31 +0200

    Added tests for Class Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item_fixture():
+    return Item("Смартфон", 10000, 20)
+
+
+def test_calculate_total_price(item_fixture):
+    assert item_fixture.calculate_total_price() == 200000
+
+
+def test_apply_discount(item_fixture):
+    Item.pay_rate = 0.8
+    item_fixture.apply_discount()
+    assert item_fixture.price == 8000.0
