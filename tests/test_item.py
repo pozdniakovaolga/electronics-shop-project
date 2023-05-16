cde32237a8716a6d0d740c9936ad0d53b9d26e83
pozdniakova olga <caramelca07@mail.ru>
2023-05-16 19:38:34 +0200

    Added new tests for class Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -7,6 +7,26 @@ def item_fixture():
     return Item("Смартфон", 10000, 20)
 
 
+@pytest.fixture
+def item_fixture_2():
+    return Item("Телефон", 10000, 5)
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+
+def test_adding_name(item_fixture_2):
+    item_fixture_2.name = "Смартфон"
+    assert item_fixture_2.name == "Смартфон"
+
+
+def test_adding_name_2(item_fixture_2):
+    item_fixture_2.name = "СуперСмартфон"
+    assert item_fixture_2.name == "Телефон"
+
+
 def test_calculate_total_price(item_fixture):
     assert item_fixture.calculate_total_price() == 200000
 
@@ -15,3 +35,9 @@ def test_apply_discount(item_fixture):
     Item.pay_rate = 0.8
     item_fixture.apply_discount()
     assert item_fixture.price == 8000.0
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
